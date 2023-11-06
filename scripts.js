b0bd8b370c7156a806eee12a04c6f4f111ae6b0a
TroubledDreamer <carlyonja@gmail.com>
2023-11-05 23:02:15 -0500

    completed project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+
+const input = document.getElementById("email");
+const form = document.querySelector("form");
+const message = document.querySelector(".message");
+
+
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const email = input.value.trim();
+
+
+    if (email!= ""){
+        message.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    }
+    else {
+        message.textContent = `Please enter a valid email address.`;
+
+    }
+
+
+
+    });
+
+});
